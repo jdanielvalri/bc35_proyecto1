d84d599391d54ee321bc718d2c9f59e5078a535a
Julio Daniel Valenzuela Rivas <jvalenri@emeal.nttdata.com>
2022-09-15 20:48:19 -0500

    proyecto base

---
+++ prjProyecto1/src/main/java/pe/com/nttdata/PrjProyecto1Application.java
@@ -0,0 +1,13 @@
+package pe.com.nttdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjProyecto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjProyecto1Application.class, args);
+	}
+
+}
